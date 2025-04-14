5fb71e8ac1bc1a1f8c219807c8bf48c9f72a8d2f
Laurence Renevier <Laurence.renevier@Lapiscine.pro>
2025-04-14 15:19:17 +0200

    projet

--- Reservation.model.php
+++ Reservation.model.php
@@ -22,14 +22,14 @@ class reservation {
  public $cleaningOption;
 
  // contient toute la structure de la reservation. je tape le code une fois et je l'utilise plusieurs fois.
-public function_construct() {
+public function _construct() {
 
 
  //récupération de la valeur.
  $this->name = "Laurence";
  $this->place= "Château de Chambord";
- $this->startDate = new DateTime("25.04.14");
- $this->endDate = new DateTime("25.05.18");
+ $this->startDate = new DateTime("25-04-14");
+ $this->endDate = new DateTime("25-05-18");
  $this->cleaningOption = true;
 
  $this->nightPrice = 1000;
